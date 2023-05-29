013dcb173c4f3910e0ab3e6ab9de889703e62a74
Brent Gorwin <brentgorwin@Brents-MacBook-Pro.local>
2023-05-29 13:10:17 -0500

    This now uses markdown files to store the content

--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,37 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Image from "next/image";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
+
+export default function Home({ allPostsData }) {
 	return (
-		<Layout>
+		<Layout home>
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
-			<section className='flex'>
-				<div className='columns-2'>
-					<Image
-						src='/../public/images/profile.jpg'
-						width={400}
-						height={400}
-					/>
-				</div>
-				<p>Hi, I'm Brent and I'm a Software Engineer!</p>
-				<p>
-					(This is a sample website - you’ll be building a site like
-					this on{" "}
-					<a href='https://nextjs.org/learn'>our Next.js tutorial</a>
-					.)
-				</p>
+
+			<section className='container '>
+				<h2 className='text-xl pb-4'>Blog</h2>
+				<ul className='list-inside '>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className='list-inside mb-3' key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
 			</section>
 		</Layout>
 	);
