85668718dcccb30b3b05b186b3360c56ceaad9d6
Brent Gorwin <brentgorwin@brents-mbp.lan>
2023-06-06 16:04:58 -0500

    Testing upload to main

--- components/navbar.js
+++ components/navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
 						href='/'
 						className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
 					>
-						Home
+						Blog Home
 					</Link>
 					{/* Add more mobile menu links as needed */}
 				</div>
